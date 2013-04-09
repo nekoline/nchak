bb26d20888ac1a53a61603eb3189df0344e681a1
nekoline <jangaly@gmail.com>
2013-04-09 17:24:50 +0900

    install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nchak::Application.config.secret_token = 'e9fbb9cc9a674db359d5216d4d07cd3fe638fab27041632d794e7fec1bcaa5e59e4ac91376ea4c1bdbcb6b3eae2eea267b61029613a41330e3bb6f5e234708e1'
