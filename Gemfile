9cc0b46a910e3b81df1eba5ec08c48ae5238ecb9
nekoline <jangaly@gmail.com>
2013-04-12 17:58:51 +0900

    devise install

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 gem 'will_paginate', '~> 3.0'
 
+gem 'devise'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Nchak::Application.routes.draw do
+  root :to => 'welcome#index'
+
+  devise_for :users
+
+  get 'tasks', :to => 'tasks#index', :as => :user_root
+
   resources :tasks, :only => [:index, :create] do
     put :finish, :on => :member
     put :unfinish, :on => :member
