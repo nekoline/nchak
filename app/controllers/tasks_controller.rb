f01e2c892a73a29ef16b358452c9ce42778f3b10
nekoline <jangaly@gmail.com>
2013-04-15 15:26:45 +0900

    devise まで

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @task  = Task.new
     # @tasks = Task.all(:conditions => {:done => false}, :order => 'due_date')
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Nchak::Application.routes.draw do
   root :to => 'welcome#index'
 
   devise_for :users
-
   get 'tasks', :to => 'tasks#index', :as => :user_root
 
   resources :tasks, :only => [:index, :create] do
